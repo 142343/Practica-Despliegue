6d88a24047f3303a60d6d9b99a070e01a3b586b1
Nayarit Gutierrez Nieto <165836550+142343@users.noreply.github.com>
2025-05-30 14:17:43 -0500

    Delete Boostrap directory

--- Boostrap/Modelo/Conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-function Conectarse()
-{
-    $conexion = mysqli_connect('localhost', 'root', '', 'paty_sport');
-    if (!$conexion) {
-        die("Conexión fallida: " . mysqli_connect_error());
-    }
-    return $conexion;
-}
