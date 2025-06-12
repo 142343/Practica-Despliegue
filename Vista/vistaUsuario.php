3d3c4d1e59dca11352bb9fc7cf65f592cb235994
Nayarit Gutierrez Nieto <165836550+142343@users.noreply.github.com>
2025-06-12 14:42:55 -0500

    Update vistaUsuario.php

--- Vista/vistaUsuario.php
+++ Vista/vistaUsuario.php
@@ -197,7 +197,7 @@
 
 <!-- Fin de encabezado-->
 <br>
-<center>
+
     <div class="breadcrumbs shadow-1">
         <div class="container-o">
             <div class="row" style="background-color: #FFFFFFFF;">
@@ -248,11 +248,11 @@
             <h5 class="offcanvas-title text-center" id="offcanvasNavbarLabel" style="font-family: 'Poppins', sans-serif; color: black; font-size: 2rem; font-weight: bold; text-shadow: 0 0 10px rgba(0, 0, 0, 0.7);">Administrador</h5>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" style="background-color: #1e3a8a;"></button>
         </div>
-        <center>
+        
             <div class="text-center">
                 <img src="https://cdn.icon-icons.com/icons2/38/PNG/512/administrator_4960.png" class="rounded-circle shadow-lg" style="width: 250px; height: 240px; border: 5px solid #1e3a8a;" alt="...">
             </div>
-            <center>
+            
                 <div class="offcanvas-body" style="margin-top: 40px;">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 
@@ -275,7 +275,7 @@
                         </li>
 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-family: 'Roboto', sans-serif; color: black;">
+                            <a>
                                 <i class="fas fa-cog"></i> Panel de Administración
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
