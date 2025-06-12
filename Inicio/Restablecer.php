6cd9d7426ecc2db3ce995239c4d64a9b70e62c29
Nayarit Gutierrez Nieto <165836550+142343@users.noreply.github.com>
2025-06-12 11:32:21 -0600

    Update Restablecer.php

--- Inicio/Restablecer.php
+++ Inicio/Restablecer.php
@@ -4,7 +4,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Conexión a la base de datos
-$conexion = new mysqli("localhost", "root", "", "paty_sport");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
