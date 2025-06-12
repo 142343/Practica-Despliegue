337cc32f5490b4bbb5f563c513253f290d2a07d3
Nayarit Gutierrez Nieto <165836550+142343@users.noreply.github.com>
2025-06-12 15:15:13 -0500

    Update ControladorIngreso.php

--- Controlador/ControladorIngreso.php
+++ Controlador/ControladorIngreso.php
@@ -1,15 +1,14 @@
 <?php
-require_once '../Modelo/Ingreso.php';
+// Cargar el autoloader de Composer si estás usando uno, o asegúrate de tener un autoload configurado
+// require_once '../vendor/autoload.php'; // (si usas Composer)
+
+use Modelo\Ingreso;
 
 $gestorIngreso = new Ingreso();
-
 $elegirAcciones = isset($_POST['Acciones']) ? $_POST['Acciones'] : "Cargar";
 
 try {
-   
     if ($elegirAcciones == 'Agregar Ingreso') {
-        
-
         if (
             isset($_POST['FechaIngresoProducto']) &&
             isset($_POST['ProductoCodigoProducto']) &&
@@ -22,23 +21,20 @@ try {
                 $_POST['FechaIngresoProducto'],
                 $_POST['ProductoCodigoProducto'],
                 $_POST['Cantidad'],
-                $_POST['Descripción'],
+                $_POST['Descripcion'],
                 $_POST['PrecioIngreso'],
                 $_POST['Empleado']
             );
         } else {
             throw new Exception("Faltan datos para agregar ingreso");
         }
-    }
-    
-   
-    elseif ($elegirAcciones == 'Actualizar Ingreso') {
+    } elseif ($elegirAcciones == 'Actualizar Ingreso') {
         if (
             isset($_POST['idTicketIngreso']) &&
             isset($_POST['FechaIngresoProducto']) &&
             isset($_POST['ProductoCodigoProducto']) &&
             isset($_POST['Cantidad']) &&
-            isset($_POST['Descripción']) &&
+            isset($_POST['Descripcion']) &&
             isset($_POST['PrecioIngreso']) &&
             isset($_POST['PrecioTotal']) &&
             isset($_POST['Empleado'])
@@ -48,7 +44,7 @@ try {
                 $_POST['FechaIngresoProducto'],
                 $_POST['ProductoCodigoProducto'],
                 $_POST['Cantidad'],
-                $_POST['Descripción'],
+                $_POST['Descripcion'],
                 $_POST['PrecioIngreso'],
                 $_POST['PrecioTotal'],
                 $_POST['Empleado']
@@ -56,31 +52,23 @@ try {
         } else {
             throw new Exception("Faltan datos para actualizar ingreso");
         }
-    }
-    
-    
-    elseif ($elegirAcciones == 'Borrar Ingreso') {
+    } elseif ($elegirAcciones == 'Borrar Ingreso') {
         if (isset($_POST['idTicketIngreso'])) {
             $gestorIngreso->borrarIngreso($_POST['idTicketIngreso']);
         } else {
             throw new Exception("Falta el ID del ingreso a borrar");
         }
-    }
-    
-    
-    elseif ($elegirAcciones == 'Buscar Ingreso') {
+    } elseif ($elegirAcciones == 'Buscar Ingreso') {
         if (isset($_POST['idTicketIngreso'])) {
             $resultado = $gestorIngreso->consultaringreso($_POST['idTicketIngreso']);
         } else {
             throw new Exception("Falta el ID del ingreso a buscar");
         }
     }
-    
-    
+
     $resultado = $gestorIngreso->consultaringreso();
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
 
 include_once "../Vista/VistaIngreso.php";
-?>
